f237d99d2707d2dc7237ccd97dd0142f33d74990
Coyote1983hg <153317330+Coyote1983hg@users.noreply.github.com>
2024-09-04 12:22:46 +0200

    commit change

--- src/App.jsx
+++ src/App.jsx
@@ -58,9 +58,91 @@ const App = () => {
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {/* Job Listing1 */}
+            <div className="bg-white p-6 rounded-lg shadow-md relative">
+              <div className="p-4">
+                <div className="mb-6">
+                  <div className="text-gray-600 my-2">Full-Time</div>
+                  <h3 className="text-xl font-bold">Senior React Developer</h3>
+                </div>
+                <div className="mb-5">We are seeking Senior React Developer to join our team in Boston,MA.
+                  The ideal candidate will have strong skills in HTML,CSS,and JavaScript...
+                  </div>
+                  <h3 className="text-indigo-500 mb-2">$70-$80k /Year</h3>
+                  <div className="border border-gray-100 mb-5"></div>
+                  <div className="flex flex-col lg:flex-row justify-between mb-4">
+                    <div className="text-orange-700 mb-4">
+                      <i className="fa-solid fa-location-dot text-lg"></i>
+                      Boston,MA
+                    </div>
+                    <a href="job.html"
+                    className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                    >
+                      Read More
+                      </a>
+                  </div>
+              </div>
+            </div>
+
+             {/* Job Listing2 */}
+             <div className="bg-white rounded-xl shadow-md relative">
+              <div className="p-4">
+                <div className="mb-6">
+                  <div className="text-gray-600 my-2">Remote</div>
+                  <h3 className="text-xl font-bold">Front-End Engineer</h3>
+                </div>
+                <div className="mb-5">
+                  Join our team as a Front-End Developer in sunny Miami,FL.We are looking for a motivated individual with passion...
+                  </div>
+                  <h3 className="text-indigo-500 mb-2">$70-$80k /Year</h3>
+                  <div className="border border-gray-100 mb-5"></div>
+                  <div className="flex flex-row justify-between mb-4">
+                    <div className="text-orange-700 mb-4">
+                      <i className="fa-solid fa-location-dot text-lg"></i>
+                      Miami,FL
+                    </div>
+                    <a href="job.html"
+                    className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm">
+                      Read More
+                      </a>
+                  </div>
+              </div>
+             </div>
+              {/* Job Listing2 */}
+              <div className="bg-white rounded-xl shadow-md relative">
+                <div className="p-4">
+                  <div className="mb-6">
+                    <div className="text-gray-600 my-2">Remote</div>
+                    <h3 className="text-xl font-bold">React.js Developer</h3>
+                  </div>
+                  <div className="mb-5">
+                    Are you passionate about frontend development? 
+                    Join our team in in vibrant Brooklyn,NY,and work and exciting projects that make a difference...
+                  </div>
+                  <h3 className="text-indigo-500 mb-2">$70-80k /Year</h3>
+                  <div className="border border-gray-100 mb-5"></div>
+                  <div className="flex flex-col lg:flex-row justify-between mb-4">
+                    <div className="text-orange-700 mb-4">
+                      <i className="fa-solid fa-location-dot text-lg"></i>
+                      Brooklyn,NY
+                    </div>
+                    <a href="job.html"
+                    className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                    >
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
+
           </div>
         </div>
       </section>
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a href="jobs.html"
+        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View all jobs</a>
+      </section>
+
+      
       <section className="m-auto max-w-lg my-10 px-6">
         <a href="jobs.html" className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View all jobs</a>
       </section>
